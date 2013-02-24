c080947baf4de411cf8381158881d06c8bc0e47b
Hiroki <1127785+azulamb@users.noreply.github.com>
2013-02-24 14:40:59 +0900

    First commit.

---
+++ SampleShooting/src/net/azulite/sampleshooting/SampleShooting.java
@@ -0,0 +1,16 @@
+package net.azulite.sampleshooting;
+
+import android.os.Bundle;
+import android.app.Activity;
+import net.azulite.Amanatsu.*;
+
+public class SampleShooting extends Activity
+{
+
+  @Override
+  protected void onCreate( Bundle savedInstanceState )
+  {
+    super.onCreate( savedInstanceState );
+    setContentView( Amanatsu.autoRunAmanatsu( this, new GameView() ) );
+  }
+}
